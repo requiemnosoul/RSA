ae06ccb033f39eb0556af14ce5f6f3758c7380d2
Valeriy <requiemnosoul@gmail.com>
2020-07-15 04:23:55 +0500

    Initial commit

---
+++ kmz6/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace kmz6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 122);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(341, 199);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Crypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(184, 327);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(359, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 199);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "<---";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(405, 122);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(290, 199);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "p:=";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox8);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(683, 87);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Key for RSA";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(532, 51);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(133, 22);
+            this.textBox8.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(393, 51);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(133, 22);
+            this.textBox6.TabIndex = 7;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(532, 23);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(133, 22);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(393, 23);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(133, 22);
+            this.textBox5.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(270, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Закрытый ключ:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(270, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Открытый ключ:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(149, 51);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Random Key";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(149, 23);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Рассчитать ключ";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(43, 51);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(43, 23);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "q:=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Исходные данные:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(402, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Обратные данные:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 359);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "RSA";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ kmz6/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kmz6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        string Crypt(long n, long exp, string text)
+        {
+            double block = (double)(text.Length*3)/ n.ToString().Length;
+            block = Math.Ceiling(block);
+            byte[] By = Encoding.GetEncoding(1251).GetBytes(text);
+            string[] ByStr = new string[By.Length];
+            for (int z = 0; z < By.Length; z++)
+            {
+                ByStr[z] = By[z].ToString();
+            }
+            for (int z = 0; z < By.Length; z++)
+            {
+                while(ByStr[z].Length < 3)
+                    ByStr[z] = "0" + By[z].ToString();
+            }
+            string txt = "";
+            for (int z = 0; z < ByStr.Length; z++)
+            {
+                txt += ByStr[z];
+            }
+            List<long> B = new List<long>();
+            string str = "";
+            for (int i = 0; i < ByStr.Length; i++)
+            {
+                if ((str + ByStr[i]).Length > n.ToString().Length)
+                {
+                    B.Add(Convert.ToInt64(str));
+                    str = ByStr[i];
+                }
+                else
+                {
+                    str += ByStr[i];
+                    if (i == ByStr.Length - 1)
+                        B.Add(Convert.ToInt64(str));
+                }
+            }
+            //int t = 0;
+            //for (int i = 0; i < txt.Length; i++)
+            //{
+            //    if (Convert.ToInt64(str + txt[i]) >= n || (str + txt[i]).Length > n.ToString().Length)
+            //    {
+            //        B.Add(Convert.ToInt64(str));
+            //        t++;
+            //        str = txt[i].ToString();
+            //    }
+            //    else
+            //    {
+            //        str += txt[i];
+            //        if (i == txt.Length - 1)
+            //            B.Add(Convert.ToInt64(str));
+            //    }
+            //}
+            
+            long[] M = new long[B.Count];
+            for (int z = 0; z < M.Length; z++)
+            {
+                M[z] = (long) BigInteger.ModPow(B[z],exp,n);
+            }
+            string shifr = "";
+            for (int z = 0; z < M.Length; z++)
+            {
+                shifr += M[z].ToString("x");
+                if (z != M.Length - 1)
+                    shifr += "-";
+            }
+            return shifr;
+        }
+        string Decrypt(long n, long d, string shifr)
+        {
+            string [] Btxt = shifr.Split('-');
+            //long [] M = new string[Btxt.Length];
+            string text = "";
+            //string str = "";
+            string str1 = "";
+            for (int i = 0; i < Btxt.Length; i++)
+            {
+                string str = BigInteger.ModPow(Convert.ToInt64(Btxt[i],16),d,n).ToString();
+                if (str.Length % 3 != 0)
+                    str = "0" + str;
+                str1 += str;
+            }
+            //for (int i = 0; i < M.Length; i++)
+            //{
+            //    str += M[i].ToString();
+            //    if(i != M.Length - 1)
+            //        if (str.Length < n.ToString().Length && Convert.ToInt64(str+M[i+1].ToString()[0]) < n)
+            //            str = "0" + str;
+            //    str1 += str;
+            //    str = "";
+            //}
+            //int t = 0;
+            byte[] by = new byte[str1.Length/3];
+            for (int i = 0; i < by.Length; i++)
+            {
+                //string s = "";
+                //int a = 3;
+                //if (Convert.ToInt32(Convert.ToString(str1[i * 3 - t]) + Convert.ToString(str1[i * 3 - t + 1]) + Convert.ToString(str1[i * 3 - t + 2])) > 255)
+                //{ a = 2; t++; }
+                //for (int j = 0; j < a; j++)
+                //{
+                //    s += str1[(i*3)-t+j];
+                //}
+                by[i] = Convert.ToByte(str1.Substring(i*3,3));
+            }
+            text = Encoding.GetEncoding(1251).GetString(by);
+            return text;
+        }
+        void Key(int bit)
+        {
+            long p = JustGen(bit);
+            long q = JustGen(bit);
+            long n = p * q;
+            long fn = eiler(p, q);
+            long exp = RandomExp(fn);
+            long d =  Obratn(exp,fn);
+            textBox3.Text = p.ToString();
+            textBox4.Text = q.ToString();
+            textBox5.Text = d.ToString();
+            textBox6.Text = exp.ToString();
+            textBox7.Text = n.ToString();
+            textBox8.Text = n.ToString();
+        }
+        long Obratn(long a, long m)
+        {
+            long x, y;
+            long g = GCD(a, m, out x, out y);
+            if (g != 1)
+                throw new ArgumentException();
+            return (x % m + m) % m;
+        }
+        long GCD(long a, long b, out long x, out long y) // Для нахождения обратного числа по модулю
+        {
+            if (a == 0)
+            {
+                x = 0;
+                y = 1;
+                return b;
+            }
+            long x1, y1;
+            long d = GCD(b % a, a, out x1, out y1);
+            x = y1 - (b / a) * x1;
+            y = x1;
+            return d;
+        }
+        long RandomExp(long x)
+        {
+            string str = x.ToString();
+            string str1;
+            long d;
+            do
+            {
+                str1 = "";
+                for (int i = 0; i < str.Length; i++)
+                    str1 += rnd.Next(0, 10).ToString();
+                d = Convert.ToInt64(str1);
+            }
+            while (d >= x || NOD(d, x) != 1);
+            return d;
+        }
+        long JustGen(int bit) // Генерация бит для простого числа
+        {
+            string proBin = "";
+            for (int i = 0; i < bit; i++)
+            {
+                proBin += rnd.Next(0, 2);
+            }
+            proBin = "1" + proBin;
+            long x = Convert.ToInt64(proBin, 2);
+            if (x % 2 == 0)
+                x++;
+            x = just(x);
+            return x;
+        }
+        long just(long x) // Проверка числа на простоту и 
+        {
+            int[] er = Eratosfen(256);
+            for (int i = 1; i < er.Length; i++)
+            {
+                if (x % er[i] == 0)
+                {
+                    x += 2;
+                    i = 0;
+                }
+            }
+            bool b = MilRab(x);
+            while (b == false)
+            {
+                x++;
+                b = MilRab(x);
+            }
+            return x;
+        }
+        bool MilRab(long n) // Тест Миллера-Рабина на простое число
+        {
+            long a, s = 0;
+            long k = Convert.ToInt32(Math.Log(n,2));
+            long t = n - 1;// Convert.ToInt32((n - 1) / (Math.Pow(2,s)));
+            while (t % 2 == 0)
+            {
+                t /= 2;
+                s++;
+            }
+            for (int i = 0; i < k; i++)
+            {
+                a = rnd.Next(2,(int) n-1);
+                BigInteger o = BigInteger.Pow(48,47);
+                o = BigInteger.ModPow(20*o,1,53);
+                long x = (long) BigInteger.ModPow(a, t, n);
+                if (x == 1 || x == n - 1)
+                    continue;
+                for (int j = 0; j < s-1; j++)
+                {
+                    x = (long) BigInteger.ModPow(x, 2, n);
+                    if (x == 1)
+                        return false;
+                    if (x == n - 1)
+                        break;
+                }
+                if(x != n-1)
+                    return false;
+            }
+            return true;
+        }
+        string SpM(int a, int t, int n) // Возведение в степень по модулю
+        {
+            //double tmp = a;
+            //string step = Convert.ToString(t, 2)+"1";
+            //int[] arr = new int[step.Length-2];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (step[i+1].ToString() == "0")
+            //        tmp = Math.Pow(tmp, 2)%n;
+            //    else
+            //        tmp = (Math.Pow(tmp, 2)*a)%n;
+            //}
+            return "";
+        }
+        long NOD(long a, long b)
+        {
+            while (b != 0)
+                b = a % (a = b);
+            return a;
+        }
+        long eiler(long p, long q)
+        {
+            long f = (p - 1) * (q - 1);
+            return f;
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            int bit = rnd.Next(23,27);
+            Key(bit);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            long n = Convert.ToInt64(textBox7.Text);
+            long exp = Convert.ToInt64(textBox5.Text);
+            string text = textBox1.Text;
+            textBox2.Text = Crypt(n,exp,text);
+        }
+
+
+        int [] Eratosfen(int n) // решето Эратосфена: выводит массив простых чисел до n
+        {
+            bool[] A = new bool[n+1];
+            int z = 0;
+            for (int i = 2; (i * i) <= n; i++)
+                if (A[i]==false)
+                    for (int j = i * i; j <= n; j += i)
+                    {
+                        if (A[j] == false)
+                        {
+                            A[j] = true;
+                            z++;
+                        }
+                    }
+            int[] B = new int[n-z-1];
+            z = 0;
+            for (int i = 2; i < n+1; i++)
+            {
+                if (A[i]==false)
+                {
+                    B[z] = i;
+                    z++;
+                }
+            }
+            return B;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            long n = Convert.ToInt64(textBox8.Text);
+            long d = Convert.ToInt64(textBox6.Text);
+            string shifr = textBox1.Text;
+            textBox2.Text = Decrypt(n, d, shifr);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox2.Text;
+            textBox2.Clear();
+        }
+    }
+}
